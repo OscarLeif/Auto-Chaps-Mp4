79c08ed580f6ca1b46aaf20e058c5c76ef4f3c63
Oscar Leif <oscar.leif@gmail.com>
2017-09-03 22:28:35 -0500

    Innitial commit, this could be the fourth time that I destroy my own project, this time I will save it in the internet. I hope this could be usefull to someone, I just upload the project so nothing works right now.

---
+++ AutoChapsMp4/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System;
+using System.Drawing;
+using System.IO;
+using Microsoft.Win32;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace AutoChapsMp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSetup_Click(object sender, RoutedEventArgs e)
+        {
+            bool isWindowOpen = false;
+
+            //Avoid Creating the same window setting
+            foreach (Window w in Application.Current.Windows)
+            {
+                if (w is SettingsWindow)
+                {
+                    isWindowOpen = true;
+                    w.Activate();
+                }
+            }
+
+            if (!isWindowOpen)
+            {
+                SettingsWindow win = new SettingsWindow();
+                win.Show();
+            }
+        }
+
+        private void ButtonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                MessageBox.Show("You selected: " + dialog.FileName);
+                textBoxPath.Text = dialog.FileName;
+            }
+        }
+    }
+}
