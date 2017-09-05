3aacd58db8852622683822eee8b1abaa0766f4c7
Oscar Leif <oscar.leif@gmail.com>
2017-09-04 20:04:03 -0500

    Added tag lib sharp and mp4Chaps.exe

--- AutoChapsMp4/MainWindow.xaml.cs
+++ AutoChapsMp4/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using Microsoft.Win32;
@@ -13,9 +14,22 @@ namespace AutoChapsMp4
     /// </summary>
     public partial class MainWindow : Window
     {
+        //The folder to fix the mp4 files
+        public String FolderPath { get; set; }
+
+        //The location of the mp4Chaps.exe
+        public String mp4Chaps;
+
+        public String commandString { get; set; }
+        public String argumentChap { get; set; }
+
+        //This can handle the external .exe files
+        private Process Process;
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadMp4Chaps();
         }
 
         private void ButtonSetup_Click(object sender, RoutedEventArgs e)
@@ -39,6 +53,16 @@ namespace AutoChapsMp4
             }
         }
 
+        public void LoadMp4Chaps()
+        {
+            //This should be loaded in the bin folder
+            mp4Chaps = "mp4chaps.exe";
+            //System.Diagnostics.Process.Start(mp4Chaps);
+            File.Exists(mp4Chaps);
+            Debug.Write("Mp4 Chaps exist in the project");
+        }
+
+
         private void ButtonBrowse_Click(object sender, RoutedEventArgs e)
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
@@ -47,8 +71,59 @@ namespace AutoChapsMp4
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 MessageBox.Show("You selected: " + dialog.FileName);
-                textBoxPath.Text = dialog.FileName;
+                FolderPath = dialog.FileName;
+                textBoxPath.Text = FolderPath;
             }
+        }
+
+        private void createChapters()
+        {
+            //First get all the mp4 file from the 
+            foreach (string file in Directory.GetFiles(FolderPath, "*mp4"))
+            {
+                Process = new Process();
+                //Begining The Mp4 Chaps Exe
+                Process.StartInfo.FileName = mp4Chaps;
+                // Add the arguments create every 600 seconds a chapter in the mp4 files
+                Process.StartInfo.Arguments = "-e 600 \"" + file + "\"";
+                //Start the Exe mp4Chaps
+                Process.Start();
+                //But we start the mp4chaps only one by one this will
+                //avoid starting multiple instances of the mp4chaps
+                Process.WaitForExit();
+
+                //Fix file name
+                FixFileName();
+
+                //UpdateMetadaName();
+
+            }
+        }
+
+        //This requieres TagLib (AKA TagSharp Lib)
+        public void UpdateMetadaName(String name, String PathFile)
+        {
+            var file = TagLib.File.Create(PathFile);
+            file.Tag.Title = name;
+            file.Save();
+        }
+
+        public void FixFileName()
+        {
+
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            //Check if the directory path is ready
+
+            //Check if mp4Chaps exist
+
+            //Start creating the job
+
+            createChapters();
+
+
         }
     }
 }
